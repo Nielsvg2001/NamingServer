dbf4751649c7b4acc34bd6581b7db4cb52be1328
Niels Van Gorp <vangorpniels@gmail.com>
2022-03-29 14:26:59 +0200

    Namingserver: "werkend" extra testen nodig + response expeptions in de controller

--- src/main/java/com/example/namingserver/Naming.java
+++ src/main/java/com/example/namingserver/Naming.java
@@ -9,33 +9,39 @@ import java.util.HashMap;
 public class Naming {
     static HashMap<Integer, Inet4Address> nodesList = new HashMap<>();
 
-    public Naming() {}
-
-    public Inet4Address getRequest(String fileName){
-        int hash = (int) ((fileName.hashCode()+2147483648.0)*(32768/(2147483648.0+Math.abs(-2147483648))));
-        Inet4Address IPaddress = checkID(hash);
-        return IPaddress;
+    public Naming() {
     }
 
-    public void addNode(String hostName, Inet4Address ipadres){
-        int hash = (int) ((hostName.hashCode()+2147483648.0)*(32768.0 /(2147483648.0 +Math.abs(-2147483648.0))));
+    public static Inet4Address getRequest(String fileName) {
+        int hash = hashCode(fileName);
+        System.out.println("Requesting file: " + fileName + " with hash: " + hash);
+        return checkID(hash);
+    }
+
+    public static int addNode(String hostName, Inet4Address ipadres) {
+        int hash = hashCode(hostName);
         nodesList.put(hash, ipadres);
+        return hash;
     }
 
-
-    public Inet4Address checkID(int hash) {
-        Integer closestID = -1;
-        for (Integer ID : this.nodesList.keySet()) {
-            if (hash < ID) {
-                closestID = ID;
+    public static Inet4Address checkID(int hashFilename) {
+        int closestID = -1;
+        int biggestID = -1;
+        for (Integer ID : nodesList.keySet()) {
+            // N is the collection of nodes with a hash smaller than the hash of the filename
+            if (ID < hashFilename) {
+                // The node with the smallest difference between its hash  and the file hash is the owner of the file.
+                if (Math.abs(ID - hashFilename) < Math.abs(closestID - hashFilename) || closestID == -1)
+                    closestID = ID;
             }
-            else {
-                Integer min = 32769;
-                if (Math.abs(hash-ID) < min) {
-                    min = ID;
-                }
-            }
+            // If N is empty, the node with the biggest hash stores the requested file
+            if (ID > biggestID)
+                biggestID = ID;
         }
-        return nodesList.get(closestID);
+        return nodesList.get((closestID == -1) ? biggestID : closestID);
+    }
+
+    public static int hashCode(String toHash) {
+        return (int) ((toHash.hashCode()+2147483648.0)*(32768/(2147483648.0+Math.abs(-2147483648.0))));
     }
 }
--- src/main/java/com/example/namingserver/RestController.java
+++ src/main/java/com/example/namingserver/RestController.java
@@ -1,18 +1,12 @@
 package com.example.namingserver;
 
 
-
-
-import java.io.IOException;
-import java.net.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -20,11 +14,20 @@ public class RestController {
     public RestController() {
     }
 
-    @GetMapping(value = "/namingRequest/")
-    public Inet4Address NamingRequest(@RequestParam String fileName){
-        System.out.println("iets");
-        Inet4Address IPaddress = Naming.getRequest(fileName);
-        return (IPaddress);
+    @PostMapping("/addNode")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addNode(@RequestParam String nodeName, @RequestParam String nodeIP) {
+        try {
+            int hash = Naming.addNode(nodeName, (Inet4Address) Inet4Address.getByName(nodeIP));
+            return "Node " + nodeName + " added with hash " + hash;
+        } catch (UnknownHostException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong IP address");
+        }
+    }
+
+    @GetMapping(value = "/namingRequest")
+    public Inet4Address NamingRequest(@RequestParam String fileName) {
+        return Naming.getRequest(fileName);
     }
 
 
