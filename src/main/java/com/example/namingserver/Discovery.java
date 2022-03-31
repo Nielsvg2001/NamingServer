9088f5d43a31f21c1223375f5b540f72e36537a4
Niels Van Gorp <vangorpniels@gmail.com>
2022-03-31 13:03:15 +0200

    enkele kleine aanpassingen

--- src/main/java/com/example/namingserver/Discovery.java
+++ src/main/java/com/example/namingserver/Discovery.java
@@ -20,23 +20,27 @@ public class Discovery {
             Thread thread = new Thread(() -> {
                 while (true) {
                     try {
+                        // Receiving new node
                         DatagramPacket packet = new DatagramPacket(new byte[256], 256);
                         datagramSocket.receive(packet);
                         String hostname = new String(packet.getData(), 0, packet.getLength());
 
+                        // Adding new node to the list
                         Naming.addNode(hostname, (Inet4Address) packet.getAddress());
 
+                        // Creating response
                         Integer hash = Naming.hashCode(hostname);
                         Integer previousNode = Naming.getNodesList().lowerKey(hash);
                         Integer nextNode = Naming.getNodesList().higherKey(hash);
 
+                        // Structuring response
                         JSONObject jsonObject = new JSONObject();
                         jsonObject.put("numberOfNodes", Naming.numberOfNodes());
                         jsonObject.put("previousNode", (previousNode == null) ? Naming.getNodesList().lastKey() : previousNode);
                         jsonObject.put("nextNode", (nextNode == null) ? Naming.getNodesList().firstKey() : nextNode);
 
+                        // Send numberOfNodes, previousNode and nextNode to the new node
                         byte[] data = jsonObject.toJSONString().getBytes();
-
                         DatagramPacket reply = new DatagramPacket(data, data.length, packet.getAddress(), packet.getPort());
                         datagramSocket.send(reply);
                     } catch (IOException e) {
