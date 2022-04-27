0ea914df103f5bb6279a84001b8d09d5d57ae87a
V-donck <tv26363@gmail.com>
2022-04-27 18:01:48 +0200

    added startup for filemanager

--- src/main/java/fileManager/Node/FileManager.java
+++ src/main/java/fileManager/Node/FileManager.java
@@ -3,17 +3,38 @@ package fileManager.Node;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 
+import java.io.File;
+import java.net.Inet4Address;
+
 public class FileManager {
+    FileTransfer fileTransfer;
 
     public FileManager() {
+        fileTransfer = new FileTransfer();
+    }
+
+    public void startUp(){
+        fileTransfer.receive();
+        File path = new File("Local_files");
+        File [] files = path.listFiles();
+        if(files!= null) {
+            for (File file : files) {
+                if (file.isFile()) { //this line weeds out other directories/folders
+                    System.out.println(file);
+                    System.out.println(namingRequest(file.getName()));
+                    fileTransfer.sendFile(namingRequest(file.getName()),file);
+                }
+            }
+        }
     }
 
 
-    public void namingRequest(String fileName) {
+    public Inet4Address namingRequest(String fileName) {
         System.out.println("request");
-        HttpResponse<String> response = Unirest.get("http://" + NetworkManager.NAMINGSERVERADDRESS + ":" + NetworkManager.NAMINGPORT + "/namingRequest")
+        HttpResponse<Inet4Address> response = Unirest.get("http://" + NetworkManager.NAMINGSERVERADDRESS + ":" + NetworkManager.NAMINGPORT + "/namingRequest")
                 .queryString("fileName", fileName)
-                .asString();
+                        .asObject(Inet4Address.class);
         System.out.println("responsebody: " + response.getBody());
+        return response.getBody();
     }
 }
