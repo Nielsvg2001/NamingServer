e802639bfd3549c96bf3dcf47685d9aae6c230a7
Max8274 <95033890+Max8274@users.noreply.github.com>
2022-05-16 08:58:37 +0200

    Added shutdown

--- src/main/java/fileManager/Node/FileManager.java
+++ src/main/java/fileManager/Node/FileManager.java
@@ -3,6 +3,8 @@ package fileManager.Node;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.DataInputStream;
 import java.io.File;
@@ -16,6 +18,7 @@ import java.util.Arrays;
 public class FileManager {
     FileTransfer fileTransfer;
     NetworkManager networkManager;
+    public static int EDGEPORT = 9995;
 
     public FileManager(NetworkManager networkManager) {
         fileTransfer = new FileTransfer(networkManager);
@@ -48,14 +51,101 @@ public class FileManager {
         return response.getBody();
     }
 
-    public void shutdown() {
+    public void shutdown() throws SocketException {
         System.out.println("shutdown! sending replicated files to previous node");
-        File path = new File("Replicated_files");
+        File path = new File("src/main/java/fileManager/Node/Replicated_files");
         File[] files = path.listFiles();
 
         assert files != null;
         for (File file : files) {
-            fileTransfer.sendFile(networkManager.getPreviousIP(), file);
+            String fileName = file.getName();
+            Inet4Address IP = checkIsALocalFile(fileName);
+            fileTransfer.sendFile(IP, file);
+        }
+    }
+
+    public Inet4Address checkIsALocalFile(String fileName) throws SocketException {
+        try {
+            DatagramSocket datagramSocket = new DatagramSocket();
+            //DatagramPacket datagramPacket = new DatagramPacket(new byte[256], 256);
+
+            byte[] bytes = fileName.getBytes();
+            while (true) {
+                try {
+                    //sending file name to previous node
+                    DatagramPacket packet = new DatagramPacket(bytes, bytes.length, networkManager.getPreviousIP(), EDGEPORT);
+                    datagramSocket.send(packet);
+
+                    packet = new DatagramPacket(new byte[256], 256);
+                    datagramSocket.receive(packet);
+                    byte[] bytes1_= packet.getData();
+                    String IP = Arrays.toString(bytes1_);
+                    Inet4Address inet4Address = (Inet4Address) Inet4Address.getByName(IP);
+                    System.out.println("Adres: " + inet4Address);
+                    return inet4Address;
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void shutdownListener() {
+        System.out.println("Listen for packets on edge port");
+        try {
+            DatagramSocket datagramSocket = new DatagramSocket(EDGEPORT);
+            while (true) {
+                Thread thread = new Thread(() -> {
+                    try {
+                        // previous node checks if he has the file as a local file
+                        DatagramPacket datagramPacket = new DatagramPacket(new byte[256], 256);
+                        datagramSocket.receive(datagramPacket);
+                        byte[] bytes = datagramPacket.getData();
+                        String fileName = bytes.toString();
+
+                        boolean isALocalFile = false;
+                        File path = new File("src/main/java/fileManager/Node/Local_files");
+                        File[] files = path.listFiles();
+                        assert files != null;
+                        for (File file: files) {
+                            if (Node.hashCode(file.getName()) == Node.hashCode(fileName)) {
+                                isALocalFile = true;
+                                break;
+                            }
+                        }
+
+                        DatagramSocket socket = new DatagramSocket();
+                        try {
+                            // if he already has the local file, send ip address of previous node to the sender
+                            if (isALocalFile) {
+                                bytes = networkManager.getPreviousIP().toString().getBytes();
+                                DatagramPacket packet = new DatagramPacket(bytes, bytes.length, datagramPacket.getAddress(), datagramPacket.getPort());
+                                socket.send(packet);
+                            }
+                            // if he doesn't have the local file, send ip address of current node to the sender
+                            else {
+                                bytes = InetAddress.getLocalHost().toString().getBytes();
+                                DatagramPacket packet = new DatagramPacket(bytes, bytes.length, datagramPacket.getAddress(), datagramPacket.getPort());
+                                socket.send(packet);
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+                thread.start();
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
         }
     }
 }
