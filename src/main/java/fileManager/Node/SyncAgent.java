5be01191c42cd0acd746be4cd8e8c97848883bf4
Max8274 <95033890+Max8274@users.noreply.github.com>
2022-05-18 17:33:34 +0200

    Update

--- src/main/java/fileManager/Node/SyncAgent.java
+++ src/main/java/fileManager/Node/SyncAgent.java
@@ -1,23 +1,58 @@
 package fileManager.Node;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.channels.FileLock;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import jade.core.Agent;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+// https://jade.tilab.com/doc/api/jade/core/Agent.html
 public class SyncAgent extends Agent {
-
-    @Override
+    private JSONArray array;
+    // werkt voor geen meter
+    /*@Override
     protected void setup() {
+        array = new JSONArray();
+    }
+
+    public void lockFileListener() {
         File path = new File("src/main/java/fileManager/Node/Replicated_files");
         File[] files = path.listFiles();
 
-        /*try (FileInputStream fis = new FileInputStream(path.toFile())) {
-            FileLock lock = fis.getChannel().lock();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }*/
-    }
+        String lockStand = "NotLocked";
+
+        for (File file : files) {
+            JSONObject jsonObject = new JSONObject();
+            for (JSONObject object : array) {
+                if (!object.get("file").equals(file)) {
+                    jsonObject.put("fileStand", lockStand);
+                    jsonObject.put("file", file);
+                    array.add(jsonObject);
+                }
+            }
+        }
+
+        while(true) {
+            Thread thread = new Thread(() -> {
+                for (JSONObject object : array) {
+                    if (object.get("fileStand").equals("Locked")) {
+                        RandomAccessFile file = (RandomAccessFile) object.get("file");
+                        FileLock lock;
+                        try {
+                            lock = file.getChannel().lock();
+                        } catch (IOException exception) {
+                            exception.printStackTrace();
+                        }
+                    }
+                    if (object.get("fileStand").equals("NotLocked")) {
+                        lock.release();
+                    }
+                }
+            });
+            thread.start();
+        }
+    }*/
 }
