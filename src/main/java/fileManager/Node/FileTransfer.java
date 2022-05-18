05d33e55469f4ec3b709cfb4bcd3236276e81063
Niels Van Gorp <vangorpniels@gmail.com>
2022-05-18 11:30:24 +0200

    added prints

--- src/main/java/fileManager/Node/FileTransfer.java
+++ src/main/java/fileManager/Node/FileTransfer.java
@@ -85,13 +85,14 @@ public class FileTransfer {
                                     if (fileContentLength > 0) {
                                         byte[] fileContentBytes = new byte[fileContentLength];
                                         dataInputStream.readFully(fileContentBytes, 0, fileContentBytes.length);
+                                        System.out.println(Arrays.toString(fileContentBytes));
                                         File fileToDownload = new File(path_ReplicationFiles + fileName);
+                                        System.out.println(fileToDownload);
                                         FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
                                         fileOutputStream.write(fileContentBytes);
                                         fileOutputStream.close();
                                         logHandler.addFileToLog(fileName, Node.hashCode(hostname), "replicated");
                                         System.out.println("in file listener filename: " + fileName);
-                                        System.out.println(Arrays.toString(fileContentBytes));
                                     }
                                 }
                             }
