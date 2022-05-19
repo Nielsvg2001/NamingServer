eb45b0b2f23a070148d0ea5203f966271bf15237
V-donck <tv26363@gmail.com>
2022-05-19 12:12:00 +0200

    added comments to RestController.java and WatchFolder.java

--- src/main/java/fileManager/Node/WatchFolder.java
+++ src/main/java/fileManager/Node/WatchFolder.java
@@ -6,58 +6,51 @@ import java.net.InetAddress;
 import java.nio.file.*;
 import java.util.Arrays;
 
-// https://fullstackdeveloper.guru/2020/12/23/how-to-watch-a-folder-directory-or-changes-using-java/
+// source: https://fullstackdeveloper.guru/2020/12/23/how-to-watch-a-folder-directory-or-changes-using-java/
 public class WatchFolder {
     public FileManager fileManager;
     public FileTransfer fileTransfer;
-
     public String localPath;
 
+    /**
+     * constructor of Watchfolder
+     * creates a folder for the local files it it does not exists and start the startWachFolder function in a new thread
+     * @param fileManagere filemanager to manage the files and to get to the filetransfer to transfer the files
+     */
     public WatchFolder(FileManager fileManagere) {
         fileManager = fileManagere;
         fileTransfer = fileManager.fileTransfer;
         localPath = "src/main/java/fileManager/Node/Local_files/";
-
         File folder = new File(localPath);
         if (!folder.exists()) {
-            folder.mkdir();
+            if(!folder.mkdir()){//create folder
+                System.out.println("Error creating directory");
+            }
         }
-
         new Thread(this::startWatchFolder).start();
     }
 
     private void startWatchFolder() {
-
         try {
-
             System.out.println("Watching directory for changes");
-
             // STEP1: Create a watch service
             WatchService watchService = FileSystems.getDefault().newWatchService();
-
             // STEP2: Get the path of the directory which you want to monitor.
             Path directory = Path.of("src/main/java/fileManager/Node/Local_files/");
-
             // STEP3: Register the directory with the watch service
             WatchKey watchKey = directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
                     StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
-
             // STEP4: Poll for events
             while (true) {
                 for (WatchEvent<?> event : watchKey.pollEvents()) {
-
                     // STEP5: Get file name from even context
                     WatchEvent<Path> pathEvent = (WatchEvent<Path>) event;
-
                     Path fileName = pathEvent.context();
                     File file = new File(directory + "/"+ fileName);
-
                     // STEP6: Check type of event.
                     WatchEvent.Kind<?> kind = event.kind();
-
                     // STEP7: Perform necessary action with the event
                     if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-
                         System.out.println("A new file is created : " + fileName);
                         System.out.println(file.getName());
                         System.out.println(file.getAbsolutePath());
@@ -78,15 +71,14 @@ public class WatchFolder {
                                 System.out.println("file send to priveous node");
                             }
                         }
-                        else{
+                        else{// send file to replicated node
                             fileTransfer.sendFile(ipaddress,file,  Node.hashCode(Inet4Address.getLocalHost().getHostName()));
                             System.out.println("file send to ipaddress");
                         }
                         System.out.println("end of watchevent file added");
                     }
-
+                    // if file deleted
                     if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
-
                         System.out.println("A file has been deleted: " + fileName);
                     }
                     /*
@@ -95,17 +87,13 @@ public class WatchFolder {
                         System.out.println("A file has been modified: " + fileName);
                     }
                      */
-
                 }
-
                 // STEP8: Reset the watch key everytime for continuing to use it for further event polling
                 boolean valid = watchKey.reset();
                 if (!valid) {
                     break;
                 }
-
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
